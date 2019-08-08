a51ce05a1907a213056b98bdd02cad1a85a299c2
naufaliqbal <90003378@medcoenergi.com>
2019-08-08 11:15:03 +0700

    asa

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist'),
         publicPath: '/'
     },
     optimization: {
